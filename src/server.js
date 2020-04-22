ca3a008e712af8a6dc18c8caa93940e52ab80679
Oscar Barajas Tavares <oscar@arepa.dev>
2020-04-22 18:37:25 -0500

    Here be Dragons

---
+++ src/server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('¡Hola!');
+});
+
+app.listen(3000, function () {
+  console.log('Servidor esta funcionando http://localhost:8000/');
+});
