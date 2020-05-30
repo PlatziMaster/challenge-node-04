ac453fcbcfcc7697c4d8c350c48ee58bc02abc86
Héctor Martínez Reséndiz <hectormartinezresendiz@gmail.com>
2020-05-29 22:35:32 -0500

    Se corrige en el archivo server.js el console log cambiando el puerto de 8000 a 3000. Se agregan rutas para agregar, listar, editar y eliminar datos.

--- src/server.js
+++ src/server.js
@@ -5,6 +5,22 @@ app.get('/', function (req, res) {
   res.send('¡Hola!');
 });
 
+app.get('/add', function (req, res) {
+  res.send('Aquí puedes colocar tu código para agregar datos a tu base');
+});
+
+app.get('/list', function (req, res) {
+  res.send('Aquí puedes colocar tu código para listar datos de tu base');
+});
+
+app.get('/edit', function (req, res) {
+  res.send('Aquí puedes colocar tu código para editar un dato de tu base');
+});
+
+app.get('/delete', function (req, res) {
+  res.send('Aquí puedes colocar tu código para eliminar datos de tu base');
+});
+
 app.listen(3000, function () {
-  console.log('Servidor esta funcionando http://localhost:8000/');
+  console.log('Servidor esta funcionando http://localhost:3000/');
 });
